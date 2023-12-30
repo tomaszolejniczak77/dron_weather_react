714224882c53f9592610e436ba491a7c67d013c8
tomaszolejniczak77 <o.tomek@wp.pl>
2023-12-30 17:54:04 +0100

    Testing improvement for weather icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import day from "../image/weather/day/113.png";
+import { useState, useEffect } from "react";
 
 const Icon = ({ iconInfo, current }) => {
   const currentWeatherCode = current.condition.code;
@@ -9,16 +9,29 @@ const Icon = ({ iconInfo, current }) => {
 
   const iconNumber = currentIcon[0].icon;
 
+  const [icon, setIcon] = useState("");
+
+  useEffect(() => {
+    const getIcon = () => {
+      import(
+        `../image/weather/${
+          current.is_day === 1 ? "day" : "night"
+        }/${iconNumber}.png`
+      ).then((image) => setIcon(image.default));
+    };
+    getIcon();
+  }, [currentWeatherCode]);
+
   return (
     <>
       <div>
-        {/* <img
-          src={`/src/image/weather/${
-            current.is_day === 1 ? "day" : "night"
-          }/${iconNumber}.png`}
-          alt="weather icon"
-        /> */}
-        <img src={day} alt="test" />
+        <img
+        // src={`src/image/weather/${
+        //   current.is_day === 1 ? "day" : "night"
+        // }/${iconNumber}.png`}
+        // alt="weather icon"
+        />
+        {icon ? <img src={icon} alt="Weather icon" /> : "Czekaj"}
       </div>
     </>
   );
