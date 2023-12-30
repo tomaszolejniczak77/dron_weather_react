38723f7883a0547da679d4f9589bbef9c92d23f7
tomaszolejniczak77 <o.tomek@wp.pl>
2023-12-30 22:39:44 +0100

    Final improvement of Leaflet icon

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -15,6 +15,8 @@ export const Map = ({
   let DefaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
   });
 
   L.Marker.prototype.options.icon = DefaultIcon;
