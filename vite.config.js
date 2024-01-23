d0d8c24775d9561228ca07785e203d6add74be51
tomaszolejniczak77 <o.tomek@wp.pl>
2024-01-23 23:27:44 +0100

    env test2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.production.APP_REACT_APP_API_KEY":
-      process.env.APP_REACT_APP_API_KEY,
-  },
 });
