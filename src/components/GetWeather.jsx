fc7584c6c58ebe89cb3c3b8176e96b973c40baa2
tomaszolejniczak77 <o.tomek@wp.pl>
2024-01-23 23:51:26 +0100

    env test3

--- src/components/GetWeather.jsx
+++ src/components/GetWeather.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 
+console.log(import.meta.env);
+
 const GetWeather = ({ coords }) => {
   const [weatherData, setWeatherData] = useState();
 
@@ -15,7 +17,7 @@ const GetWeather = ({ coords }) => {
           days: "3",
         },
         headers: {
-          "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_API_KEY,
+          "X-RapidAPI-Key": import.meta.env.APP_REACT_APP_API_KEY,
           "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
         },
       };
